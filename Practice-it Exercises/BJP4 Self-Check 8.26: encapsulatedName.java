fe1aa2454959bca3bdd50d7f7a7d7066eb472156
Kelvin Brenand <brenand.kelvin@gmail.com>
2020-05-15 00:02:34 -0300

    Create BJP4 Self-Check 8.26: encapsulatedName.java

---
+++ Practice-it Exercises/BJP4 Self-Check 8.26: encapsulatedName.java
@@ -0,0 +1,24 @@
+// A Name object represents a person's name such as "John Q. Public".
+public class Name {
+    private String firstName;
+    private char middleInitial;
+    private String lastName;
+    
+    public Name(String first, char middle, String last) {
+        firstName = first;
+        middleInitial = middle;
+        lastName = last;
+    }
+    
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    public char getMiddleInitial(){
+        return middleInitial;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+}
